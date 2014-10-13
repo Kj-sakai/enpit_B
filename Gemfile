6b48e4d7a4cd5c2947d1a6aa2b946e8d9bcf4b2c
omoto <somoto2008@gmail.com>
2014-10-13 12:58:04 +0000

    Create my_branch

--- Gemfile
+++ Gemfile
@@ -42,5 +42,5 @@ gem 'spring',        group: :development
 gem 'rails_12factor', group: :production
 ruby '2.1.2'
 
-
+gem 'youtube_search'
 
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,26 @@
+# coding: utf-8
 class WelcomeController < ApplicationController
+ 
+require 'youtube_search'
+require 'pp'
+ 
+keyword = "GLAY 函館"
+option = {
+  :per_page => "10",
+  :page     => "5",
+}
+ 
+videos = YoutubeSearch::search(keyword, option)
+ 
+p "video count is #{videos.count}"
+p "--------------------------------"
+ 
+videos.each do |video|
+  p video["video_id"]
+  p video["title"]
+  p video["content"]
+  p "---------------------------------"
+
+end
+
 end
